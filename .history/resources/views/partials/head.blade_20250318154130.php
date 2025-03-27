8bd83eb25aa15f984fa8d227406d93a28431be71
Hamza <hamzacoude@gmail.com>
2025-03-27 13:36:05 +0000

    remove outdated styles and head partials to clean up the project

--- .history/resources/views/partials/head.blade_20250318154130.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TimeTrekker - Voyage Through the Ages</title>
-    <link rel="icon" type="image/png" href="https://cdn-icons-png.flaticon.com/512/4645/4645379.png">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <link rel="stylesheet" href="{{url}}">
-</head>
