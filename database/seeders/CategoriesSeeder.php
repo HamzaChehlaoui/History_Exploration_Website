f4b53d5a83897499230bd2895a1b0c3396d44a50
Hamza <hamzacoude@gmail.com>
2025-04-02 13:24:59 +0000

    add seeder categories

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategoriesSeeder extends Seeder
 {
@@ -12,6 +13,10 @@ class CategoriesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('categories')->insert([
+            ['name' => 'History'],
+            ['name' => 'Archeology'],
+            ['name' => 'Ancient Civilizations'],
+        ]);
     }
 }
