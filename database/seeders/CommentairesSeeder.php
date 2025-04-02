166564ee05e3441d742806b75deb2daa0f3e6ec0
Hamza <hamzacoude@gmail.com>
2025-04-02 13:29:30 +0000

    add seeder commentairegit

--- database/seeders/CommentairesSeeder.php
+++ database/seeders/CommentairesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CommentairesSeeder extends Seeder
 {
@@ -12,6 +13,17 @@ class CommentairesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('commentaires')->insert([
+            [
+                'content' => 'Great article about Ancient Rome!',
+                'user_id' => 2,
+                'article_id' => 1
+            ],
+            [
+                'content' => 'I love learning about Egyptian history.',
+                'user_id' => 1,
+                'article_id' => 2
+            ],
+        ]);
     }
 }
