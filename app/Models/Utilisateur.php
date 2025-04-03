fb34fac0b9970bec0d89f621e41d1600f754bc5e
Hamza <hamzacoude@gmail.com>
2025-04-03 19:16:49 +0000

    rename User model to Utilisateur and update attributes for French naming conventions

---
+++ app/Models/Utilisateur.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Utilisateur extends Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'utilisateurs';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role_id',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+}
+
