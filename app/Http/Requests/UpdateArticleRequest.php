71d0648a50781703c6ece056c694131ab525f583
Hamza <hamzacoude@gmail.com>
2025-04-04 23:11:33 +0000

    add ArticleController and update validation rules in UpdateArticleRequest

--- app/Http/Requests/UpdateArticleRequest.php
+++ app/Http/Requests/UpdateArticleRequest.php
@@ -6,23 +6,20 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateArticleRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'title' => 'sometimes|required|string|max:255',
+            'content' => 'sometimes|required|string',
+            'description' => 'sometimes|required|string',
+            'date_publication' => 'sometimes|required|date',
+            'category_id' => 'sometimes|required|exists:categories,id',
+            'utilisateur_id' => 'sometimes|required|exists:utilisateurs,id',
         ];
     }
 }
