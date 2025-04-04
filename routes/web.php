b1d5cb3d745357a290028b812438cadfb12b8b07
Hamza <hamzacoude@gmail.com>
2025-04-04 00:06:32 +0000

    remove dashboard route middleware from web.php

--- routes/web.php
+++ routes/web.php
@@ -29,13 +29,3 @@ Route::group([] ,function (){
     Route::view('/PageConditions','Visiteur.PageConditions');
 });
 
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
