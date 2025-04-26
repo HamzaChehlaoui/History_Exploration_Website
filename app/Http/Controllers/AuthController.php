7772a834d14a60d951fba5430dfcc6a09bb3029a
Hamza <hamzacoude@gmail.com>
2025-04-26 19:24:21 +0000

    Fix typo in redirect path in register method of AuthController

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
             ]);
 
 
-            return redirect('/ogin')->with('success', 'Welcome! Registration successful.');
+            return redirect('/login')->with('success', 'Welcome! Registration successful.');
         }
     public function login(Request $request)
         {
