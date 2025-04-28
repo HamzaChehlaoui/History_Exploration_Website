b598b65b1e04262886fb7751d16d0fc75a27afc1
Hamza <hamzacoude@gmail.com>
2025-04-28 11:53:29 +0000

    Add CSRF token meta tag for enhanced security in head section

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -11,6 +11,7 @@
     <script src="https://unpkg.com/topojson-client@3"></script> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
 
 </head>
