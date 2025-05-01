a6886e761b910c1584dc973c8412a13fb5575c42
Hamza <hamzacoude@gmail.com>
2025-05-01 17:52:50 +0000

    Add imagePath for users in UtilisateursSeeder to enhance profile image handling

--- database/seeders/UtilisateursSeeder.php
+++ database/seeders/UtilisateursSeeder.php
@@ -15,12 +15,14 @@ class UtilisateursSeeder extends Seeder
                 'email' => 'john@example.com',
                 'password' => Hash::make('password'),
                 'role_id' => 1,
+                'imagePath'=>'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWQ_cwOJtJzYy_bRLR9iTPgfZC1w6YD582vg&s',
             ],
             [
                 'name' => 'Jane Smith',
                 'email' => 'jane@example.com',
                 'password' => Hash::make('password'),
                 'role_id' => 2,
+                'imagePath'=>'https://media.istockphoto.com/id/637969836/fr/photo/homme-sur-le-sommet-de-la-montagne-sur-fond-de-coucher-de-soleil.jpg?s=170667a&w=0&k=20&c=CygyD6A4dP9e-GMbzEiKSgAKXFuFComXY53L1pvFvsE=',
             ]
         ]);
     }
